ba1a5e9a381839c9b60cc0b09205df0d05839b2d
AndriiKot <a_kocyuba@ukr.net>
2024-07-03 13:13:24 +0200

    React Native 'Testin_01 'React Native''! create fn 'checkExistsDB' !

--- DB/DBusers.js
+++ DB/DBusers.js
@@ -1,3 +1,10 @@
 ﻿import * as 'SQLite' from 'expo-sqlite';
 import * as 'FileSystem' from 'expo-file-system';
 import { Configuration } from '../store/values/configuration';
+
+
+export const checkExistsDB = async (dbName) => {
+    const dbDir = FileSystem.documentDirectory + 'SQLite/';
+    const dirInfo = await FileSystem.getInfoAsync(dbDir + dbName);
+    // const db = SQLite.openDatabase(Configuration.dbName);
+}
