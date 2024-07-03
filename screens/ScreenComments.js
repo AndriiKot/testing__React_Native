5f24abab278dded3ce1d81ee0b48cc516003cc47
AndriiKot <a_kocyuba@ukr.net>
2024-07-03 14:51:25 +0200

    React Native 'Testin_01 'React Native''! CERATE ScreenCommnets.js  !

---
+++ screens/ScreenComments.js
@@ -0,0 +1,28 @@
+﻿import { useState, useContext } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StatusBar,
+  TextInput,
+} from "react-native";
+import {
+  createDB,
+  insert,
+  select,
+  deleteDB,
+  updateDB,
+  checkExistsDB,
+} from "../DB/DBusers";
+
+export default function ScreenComments() {
+  const [text, setText] = useState("");
+
+  return (
+    <View style={{ flex: 1 }}>
+      <StatusBar />
+      <TextInput onChangeText={setText}> {text}</TextInput> />
+      <Text>ScreenComments</Text>
+    </View>
+  );
+}
